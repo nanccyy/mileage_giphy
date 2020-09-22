010a98e090f25edbf1bdde6a1714ef2573e16e6b
nanccyy <lidiapriymak@gmail.com>
2020-09-22 14:23:25 +0300

    Updates

---
+++ precache-manifest.301dc17be69b7be03d8c6d54279d6a41.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d51d877370b6051333fa16c2721362",
+    "url": "/mileage__local/index.html"
+  },
+  {
+    "revision": "f2496e5c19a7435af61d",
+    "url": "/mileage__local/static/css/main.f00c74e9.chunk.css"
+  },
+  {
+    "revision": "62f26edc710151ae9892",
+    "url": "/mileage__local/static/js/2.e38a6ab8.chunk.js"
+  },
+  {
+    "revision": "bc87d5d06c5dc62879ad4e1d6ba919e4",
+    "url": "/mileage__local/static/js/2.e38a6ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2496e5c19a7435af61d",
+    "url": "/mileage__local/static/js/main.65955bc4.chunk.js"
+  },
+  {
+    "revision": "959542f4c53609da9168",
+    "url": "/mileage__local/static/js/runtime-main.9523e9e1.js"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/mileage__local/static/media/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "07e6a6cd3cdb9eedaa9aa81fafe1e42a",
+    "url": "/mileage__local/static/media/Gilroy-ExtraBold.07e6a6cd.ttf"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/mileage__local/static/media/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "05bdf30b8aaa10683c19e73dd0c428da",
+    "url": "/mileage__local/static/media/Gilroy-SemiBold.05bdf30b.ttf"
+  },
+  {
+    "revision": "cdce98e1eaf4e5b81b437f6164db7119",
+    "url": "/mileage__local/static/media/arrow_active.cdce98e1.svg"
+  },
+  {
+    "revision": "ee8239bb823bf43a343a0e8ba96ec68e",
+    "url": "/mileage__local/static/media/arrow_left.ee8239bb.svg"
+  },
+  {
+    "revision": "71b22a7ac3507ee24873aeb3c2130be5",
+    "url": "/mileage__local/static/media/arrow_right.71b22a7a.svg"
+  },
+  {
+    "revision": "d742825ebb300961723f8afe95539dc3",
+    "url": "/mileage__local/static/media/button_arrow.d742825e.svg"
+  },
+  {
+    "revision": "d97948d8d67ba62dd8ad475ca32fb981",
+    "url": "/mileage__local/static/media/check_mark_blue.d97948d8.svg"
+  },
+  {
+    "revision": "b603ad8ec0852c9de847e97d103514d1",
+    "url": "/mileage__local/static/media/image.b603ad8e.jpg"
+  },
+  {
+    "revision": "9f2dcbe50934c35c40eb484f746e4693",
+    "url": "/mileage__local/static/media/image1.9f2dcbe5.jpg"
+  },
+  {
+    "revision": "d30290e389c249788836a1a58b784020",
+    "url": "/mileage__local/static/media/image2.d30290e3.jpg"
+  },
+  {
+    "revision": "eef9037d2c97d1ff545fd838cbd8b2df",
+    "url": "/mileage__local/static/media/image3.eef9037d.jpg"
+  },
+  {
+    "revision": "e41d2c86afa970bae1f44616496590a0",
+    "url": "/mileage__local/static/media/image4.e41d2c86.jpg"
+  },
+  {
+    "revision": "7dd4e4e234d2a2a52a03c9fdba6e78c4",
+    "url": "/mileage__local/static/media/image5.7dd4e4e2.jpg"
+  },
+  {
+    "revision": "a5cdea8ab7acdd4b24f3b0323faf1684",
+    "url": "/mileage__local/static/media/logo.a5cdea8a.svg"
+  },
+  {
+    "revision": "14f8129eca8db07e61d4df1f405ac8c2",
+    "url": "/mileage__local/static/media/point_green_border.14f8129e.svg"
+  },
+  {
+    "revision": "fd9122440e73aaefc370084c331d5507",
+    "url": "/mileage__local/static/media/services1.fd912244.svg"
+  },
+  {
+    "revision": "94204eb06e0fa5380f8352138d7589b0",
+    "url": "/mileage__local/static/media/services__path.94204eb0.svg"
+  },
+  {
+    "revision": "f31f1e62c9f1e703fe17be36bf9c7ed4",
+    "url": "/mileage__local/static/media/services_bg_circle.f31f1e62.svg"
+  },
+  {
+    "revision": "81ca160b25b19d586948340a2b2f5b38",
+    "url": "/mileage__local/static/media/services_dot_active.81ca160b.svg"
+  },
+  {
+    "revision": "f5ff110d424947d1280b0b11f03d351e",
+    "url": "/mileage__local/static/media/services_img_reversed.f5ff110d.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mileage__local/precache-manifest.301dc17be69b7be03d8c6d54279d6a41.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mileage__local/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
